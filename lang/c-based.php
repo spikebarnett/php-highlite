dae19c153df50b32f3e9020f798cde0bec55ae34
Chris Barnett <spike.barnett@gmail.com>
2016-08-24 04:35:21 -0400

    moved allcaps to end of pattern arrays

--- lang/c-based.php
+++ lang/c-based.php
@@ -6,8 +6,8 @@ $highlight_regex["c"] = [
 ['code-number',   '/(.*?\W)(#\d{6}|#\d{3}|-?(?:\d+?\.\d+?|\.\d+?|\d+?)%?|0x\d+?)(\W.*)/s'],
 ['code-symbol',   "/(.*?)([<>\\\\])(.*)/s"],
 ['code-symbol',   "/(.*?)([-$!%^*()+|~=`{}[:;?,.@#&_\/\]]+)(.*)/s"],
-['code-allcaps',  '/(.*?)(\b[A-Z]+\b)(.*)/s'],
 ['code-keyword',  '/(.*?)(\b(a(?:nd|rray|s)|b(?:ool|oolean)|c(?:atch|har|lass|onst)|d(?:elete|ie|o|ouble)|e(?:lse|lseif|xit|xtends)|f(?:alse|inally|loat|or|oreach|unction)|g(?:lobal)|i(?:f|nt|nteger)|l(?:ong)|n(?:ew|ull)|o(?:bject|r)|p(?:rivate|rotected|ublic|ublished)|r(?:eal|esource|eturn)|s(?:elf|hort|igned|tatic|tring|truct|witch)|t(?:hen|his|hrow|rue|ry)|u(?:nsigned)|v(?:ar|oid)|w(?:hile)|x(?:or))\b)(.*)/s'],
+['code-allcaps',  '/(.*?)(\b[A-Z]+\b)(.*)/s'],
 ];
 $highlight_regex["cpp"] = $highlight_regex["c"];
 $highlight_regex["csharp"] = $highlight_regex["c"];
--- lang/java.php
+++ lang/java.php
@@ -6,6 +6,6 @@ $highlight_regex["java"] = [
 ['code-variable', '/(.*?)(\$\w+)(.*)/s'],
 ['code-symbol',   "/(.*?)([<>\\\\])(.*)/s"],
 ['code-symbol',   "/(.*?)([-$!%^*()+|~=`{}[:;?,.@#&_\/\]]+)(.*)/s"],
-['code-allcaps',  '/(.*?)(\b[A-Z]+\b)(.*)/s'],
 ['code-keyword',  '/(.*?)\b(a(?:bstract|ssert)|b(?:oolean|reak|yte)|c(?:ase|atch|har|lass|onst|ontinue)|d(?:efault|o|ouble)|e(?:lse|num|xtends)|f(?:inal|inally|loat|or)|g(?:oto)|i(?:f|mplements|mport|nstanceof|nt|nterface)|l(?:ong)|n(?:ative|ew)|p(?:ackage|rivate|rotected|ublic)|r(?:eturn)|s(?:hort|tatic|trictfp|uper|witch|ynchronized)|t(?:his|hrow|hrows|ransient|ry)|v(?:oid|olatile)|w(?:hile))\b(.*)/s'],
+['code-allcaps',  '/(.*?)(\b[A-Z]+\b)(.*)/s'],
 ];
