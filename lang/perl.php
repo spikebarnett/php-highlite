d490dd6c38a82bcc3c8584a4847476b540b8d9fa
Chris Barnett <spike.barnett@gmail.com>
2016-08-24 05:49:15 -0400

    Fixed regex bug

--- lang/perl.php
+++ lang/perl.php
@@ -7,6 +7,6 @@ $highlight_regex["perl"] = [
 ['code-variable', '/(.*?)([$@]\w+)(.*)/s'],
 ['code-symbol',   "/(.*?)([<>\\\\])(.*)/s"],
 ['code-symbol',   "/(.*?)([-$!%^*()+|~=`{}[:;?,.@#&_\/\]]+)(.*)/s"],
-['code-keyword',  '/(.*?)(\b(a(?:bstract|nd|s)|b(?:reak)|c(?:allable|ase|atch|lass|lone|onst|ontinue)|d(?:eclare|efault|o)|e(?:lse|lseif|nddeclare|ndfor|ndforeach|ndif|ndswitch|ndwhile|xtends)|f(?:inal|inally|or|oreach|unction)|g(?:lobal|oto)|i(?:f|mplements|nstanceof|nsteadof|nterface)|n(?:amespace|ew)|o(?:r)|p(?:rivate|rotected|ublic)|s(?:tatic|witch)|t(?:hrow|rait|ry)|u(?:se)|v(?:ar)|w(?:hile)|x(?:or)|y(?:ield))\b)(.*)/s'],
+['code-keyword',  '/(.*?)\b(a(?:bstract|nd|s)|b(?:reak)|c(?:allable|ase|atch|lass|lone|onst|ontinue)|d(?:eclare|efault|o)|e(?:lse|lseif|nddeclare|ndfor|ndforeach|ndif|ndswitch|ndwhile|xtends)|f(?:inal|inally|or|oreach|unction)|g(?:lobal|oto)|i(?:f|mplements|nstanceof|nsteadof|nterface)|n(?:amespace|ew)|o(?:r)|p(?:rivate|rotected|ublic)|s(?:tatic|witch)|t(?:hrow|rait|ry)|u(?:se)|v(?:ar)|w(?:hile)|x(?:or)|y(?:ield))\b(.*)/s'],
 ['code-allcaps',  '/(.*?)(\b[A-Z]+\b)(.*)/s'],
 ];
